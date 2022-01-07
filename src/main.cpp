8caa8a9e63b5070aef29543a7fa1d4e4d1aca7ab
April <buzdav2@gmail.com>
2022-01-06 21:06:56 -0600

    greatly simplified and optimized triangle renderer

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,55 @@
 #include "engine/engine.hpp"
 #include "engine/session.hpp"
 
-class Game : public engine::GameState {
-public:
-    void handle_update(engine::EngineController eg, Millis delta) override {
+using namespace engine;
 
-    }
-    void handle_key_down(engine::EngineController eg, engine::KeyDownEvent event) override {
-        if (event.key == engine::Scancode::Space) {
-            std::swap(start_pos, end_pos);
+class Game : public GameState {
+public:
+    void handle_update(EngineController eg, Millis delta) override {}
+    void handle_key_down(EngineController eg, KeyDownEvent event) override {}
+    void handle_key_up(EngineController eg, KeyUpEvent event) override {}
+    void handle_mouse_button_down(EngineController eg, MouseButtonDownEvent event) override {}
+    void handle_mouse_button_up(EngineController eg, MouseButtonUpEvent event) override {}
+    void handle_mouse_move(EngineController eg, MouseMoveEvent event) override {
+        auto in = eg.get_input_controller();
+        if (in.is_mouse_button_down(MouseButton::Left)) {
+            p0 = event.pos;
+        } else if (in.is_mouse_button_down(MouseButton::Right)) {
+            p1 = event.pos;
         }
     }
-    void handle_key_up(engine::EngineController eg, engine::KeyUpEvent event) override {}
-    void handle_mouse_button_down(engine::EngineController eg, engine::MouseButtonDownEvent event) override {}
-    void handle_mouse_button_up(engine::EngineController eg, engine::MouseButtonUpEvent event) override {}
-    void handle_mouse_move(engine::EngineController eg, engine::MouseMoveEvent event) override {
-        start_pos = event.pos;
-    }
 
-    void handle_graphics_update(engine::EngineController eg) const override {
+    void handle_graphics_update(EngineController eg) const override {
         auto graphics = eg.get_graphics_controller();
-        graphics.setColor({255, 0 ,0});
-        graphics.drawLine(start_pos, end_pos);
+        graphics.drawTri(p0, p1, p2,
+            [](GraphicsController &graphics, IVec2 pos, int det, int a0, int a1, int a2) {
+                graphics.setColor({uint8_t(a0 * 255 / det), uint8_t(a1 * 255 / det), uint8_t(a2 * 255 / det)});
+                graphics.drawPixel(pos);
+            });
     }
 
 private:
-    engine::IVec2 start_pos = {0, 0};
-    engine::IVec2 end_pos = {0, 0};
+    IVec2 p0 = {0, 0};
+    IVec2 p1 = {0, 0};
+    IVec2 p2 = {200, 280};
 };
 
 
 int main(int argc, char **argv) {
-    engine::InputConfig input_config {};
-    engine::GraphicsConfig graphics_config {
+    InputConfig input_config {};
+    GraphicsConfig graphics_config {
         .window_title = "Hello world!",
         .window_width = 400,
         .window_height = 300,
     };
-    engine::EngineConfig config {
+    EngineConfig config {
         .frame_delay = 1000 / 60,
         .graphics_config = graphics_config,
         .input_config = input_config
     };
 
-    engine::Session session;
-    engine::Engine engine(session, config);
+    Session session;
+    Engine engine(session, config);
     engine.run(Game());
 
     return 0;
